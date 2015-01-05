40336ba69467cae02ea3367d4a58183185281f0b
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-05 21:17:54 +0200

    added first revision of the second task (not tested yet)

---
+++ hw/2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include "stack.h"
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    queue<stack<bool> > qs;
+    stack<bool> s;
+    bool ends = false, endq = false;
+    size_t input;
+
+    do
+    {
+        cin >> input;
+
+        if (input != -1)
+        {
+            s.push(input);
+            ends = false;
+        }
+        else
+        {
+            qs.push(s);
+            destroy(s);
+
+            if (!ends)
+                ends = true;
+            else
+                endq = true;
+        }
+    }
+    while (!endq);
+
+    size_t t = 0;
+
+    while (!qs.empty())
+    {
+        cout << "***********" << endl;
+        cout << t << " минута:" << endl;
+
+        size_t i = 0;
+
+        while (!qs.empty() && s.size() < 15)
+        {
+            copy(s, qs.front());
+            cout << "Келнер " << i << " оставя чинии" << endl;
+            qs.pop();
+            i++;
+        }
+
+        size_t nt = 0, nf = 0;
+
+        while (!s.empty() && nt < 7)
+        {
+            bool c = s.top();
+            c ? nt++ : nf++;
+            s.pop();
+        }
+    
+        cout << nt << " измити чинии" << endl;
+        cout << nf << " счупени чинии" << endl;
+        t++;
+    }
+}
