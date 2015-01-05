9d44fa3672f6717e0c7b6f95caf3880f0bb70df1
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-05 21:36:10 +0200

    removed obsolete header file after parametricization of stack library

--- hw/stack.h
+++
@@ -1,5 +0,0 @@
-#include <stack>
-
-void pour(std::stack<std::size_t>&, std::stack<std::size_t>);
-void copy(std::stack<std::size_t>&, std::stack<std::size_t>);
-void destroy(std::stack<std::size_t>&);
