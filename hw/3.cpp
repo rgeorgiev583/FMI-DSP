8c36cfd0592143ca0d2d45464ddcb572f0cfe527
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-08 00:39:22 +0200

    modified join method: it uses dlchain now

--- hw/3.cpp
+++ hw/3.cpp
@@ -1,35 +1,19 @@
 #include "dllist.cpp"
+#include "dlchain.cpp"
 
 template <typename T>
-DoubleLinkedList<T> join_at(DoubleLinkedList<T> L1,
-        const DoubleLinkedList<T>& L2,
-        const DoubleLinkedListIterator<T>& M1,
-        const DoubleLinkedListIterator<T>& M2)
+DoubleLinkedChain<T> join_at(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2,
+        DoubleLinkedListIterator<T>& M1, DoubleLinkedListIterator<T>& M2)
 {
-    DoubleLinkedListIterator <T> itL2 = L2.begin();
-
-    while (itL2 && itL2 != M2)
-    {
-        L1.insertBefore(*itL2, M1);
-        itL2++;
-    }
-
-    T x;
-    L1.deleteAt(x, M1);
-    itL2++;
-
-    while (itL2)
-    {
-        L1.insertEnd(*itL2);
-        itL2++;
-    }
-
-    return L1;
+    DoubleLinkedChain<T> ch;
+    ch.adoptList(L1);
+    ch.attachListAtFront(L2, M1);
+    ch.attachAtEnd(L2, M2);
+    return ch;
 }
 
 template <typename T>
-DoubleLinkedList<T> join(const DoubleLinkedList<T>& L1,
-                         const DoubleLinkedList<T>& L2)
+DoubleLinkedChain<T> join(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2)
 {
     DoubleLinkedListIterator<T> itL1 = L1.begin(), itL2 = L2.end(), M1, M2;
 
@@ -62,3 +46,4 @@ DoubleLinkedList<T> join(const DoubleLinkedList<T>& L1,
 
     return join_at(L1, L2, M1, M2);
 }
+
