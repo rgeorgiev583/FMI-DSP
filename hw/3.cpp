d8c87cdcb444504ca6825ec9f90054ecc28de11e
rgeorgiev583 <rgeorgiev583@gmail.com>
2015-01-08 19:27:57 +0200

    added implementations of the isJoined (check if there is a chain between two element pointers) and sum (adds all elements in a chain) methods

--- hw/3.cpp
+++ hw/3.cpp
@@ -2,10 +2,10 @@
 #include "dlchain.cpp"
 
 template <typename T>
-DoubleLinkedChain<T> join_at(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2,
+DoubleLinkedChain<T>& join_at(DoubleLinkedChain<T>& ch,
+        DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2,
         DoubleLinkedListIterator<T>& M1, DoubleLinkedListIterator<T>& M2)
 {
-    DoubleLinkedChain<T> ch;
     ch.adoptList(L1);
     ch.attachListAtFront(L2, M1);
     ch.attachAtEnd(L2, M2);
@@ -13,7 +13,17 @@ DoubleLinkedChain<T> join_at(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2,
 }
 
 template <typename T>
-DoubleLinkedChain<T> join(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2)
+DoubleLinkedChain<T> join_at(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2,
+        DoubleLinkedListIterator<T>& M1, DoubleLinkedListIterator<T>& M2)
+{
+    DoubleLinkedChain<T> ch;
+    join_at(ch, L1, L2, M1, M2);
+    return ch;
+}
+
+template <typename T>
+DoubleLinkedChain<T>& join(DoubleLinkedChain<T>& ch,
+        DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2)
 {
     DoubleLinkedListIterator<T> itL1 = L1.begin(), itL2 = L2.end(), M1, M2;
 
@@ -44,6 +54,70 @@ DoubleLinkedChain<T> join(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2)
         itL2++;
     }
 
-    return join_at(L1, L2, M1, M2);
+    return join_at(ch, L1, L2, M1, M2);
 }
 
+template <typename T>
+DoubleLinkedChain<T> join(DoubleLinkedList<T>& L1, DoubleLinkedList<T>& L2)
+{
+    DoubleLinkedChain<T> ch;
+    ch.join(ch, L1, L2);
+    return ch;
+}
+
+template <typename T>
+bool isJoined(const DoubleListElement<T>* front,
+              const DoubleListElement<T>* back)
+{
+    if (!front || !back)
+        return false;
+
+    DoubleListElement<T>* pn = front->prev;
+
+    while (front && front->prev == pn)
+    {
+        pn = front;
+        front = front->next;
+    }
+
+    pn = back->next;
+
+    while (back && back->next == pn)
+    {
+        pn = back;
+        back = back->prev;
+    }
+
+    return front && back;
+}
+
+template <typename T>
+T sum(const DoubleLinkedChain<T>& ch)
+{
+    DoubleListElement<T> *front = ch.getFrontPtr(),
+                         *back  = ch.getBackPtr();
+    T S = T();
+
+    if (!front || !back)
+        return S;
+
+    DoubleListElement<T> *pn = front->prev;
+
+    while (front && front->prev == pn)
+    {
+        S += front->data;
+        pn = front;
+        front = front->next;
+    }
+
+    pn = back->next;
+
+    while (back && back->next == pn)
+    {
+        S += back->data;
+        pn = back;
+        back = back->prev;
+    }
+
+    return S;
+}
